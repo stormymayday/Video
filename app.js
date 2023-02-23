79c276b5e54c8af9e306b50cfddd365d6557164b
stormymayday <stormymayday7@gmail.com>
2023-02-22 20:51:55 -0700

    Implemented the video pause and play functionality.

--- app.js
+++ app.js
@@ -6,9 +6,20 @@ switchBtn.addEventListener('click', function () {
 
     // checking if the button has .slide class
     if (!switchBtn.classList.contains('slide')) {
+
+        // adding the slide class
         switchBtn.classList.add('slide');
+
+        // pausing the video
+        videoContainer.pause();
+
     } else {
+
+        // removing the slide class
         switchBtn.classList.remove('slide');
+
+        // playing the video
+        videoContainer.play();
     }
 
 });
