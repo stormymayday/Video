4aeeb8e286ff1e3c07faafcad8f102f85c05cd24
stormymayday <stormymayday7@gmail.com>
2023-02-22 20:48:11 -0700

    Added click event listener to the switchBtn that checks if the switchBtn has class of slide. If it does not, it adds it. Else, it removes it. Changed second span value to pause.

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 // selections
 const switchBtn = document.querySelector('.switch-btn');
 const videoContainer = document.querySelector('.video-container');
+
+switchBtn.addEventListener('click', function () {
+
+    // checking if the button has .slide class
+    if (!switchBtn.classList.contains('slide')) {
+        switchBtn.classList.add('slide');
+    } else {
+        switchBtn.classList.remove('slide');
+    }
+
+});
